9f25b4026a9afc2964a2f6a613545520946f404f
Kevin Lee <klee03509@gmail.com>
2023-08-05 00:39:11 -0700

    Update: 2023/08/05

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,6 +1,6 @@
 local ok, custom = pcall(require, 'custom.keymaps')
 if ok then
     for _, keymap in ipairs(custom) do
-        vim.keymap.set(unpack(keymap))
+        vim.keymap.set(table.unpack(keymap))
     end
 end
