e0e11f7008d2c2a677a883aba846c1915a8397be
Kevin Lee <klee03509@gmail.com>
2024-03-29 22:34:48 -0700

    Update: 2024/03/29
    - Add Metals LSP
    - Move colorschemes into custom

--- lua/plugins/lsp-zero.lua
+++ lua/plugins/lsp-zero.lua
@@ -24,39 +24,7 @@ local M = {
 M.config = function()
     local lsp_zero = require("lsp-zero")
 
-    lsp_zero.on_attach(function(client, bufnr)
-        local opts = { buffer = bufnr, noremap = true, silent = true }
-
-        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-        vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-        vim.keymap.set("n", "gs", vim.lsp.buf.signature_help, opts)
-        vim.keymap.set("n", "gl", vim.diagnostic.open_float, opts)
-        vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
-        vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
-        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-        vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, opts)
-
-        if pcall(require, "telescope") then
-            vim.keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<cr>", opts)
-            vim.keymap.set("n", "gr", "<cmd>Telescope lsp_references<cr>", opts)
-            vim.keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<cr>", opts)
-            vim.keymap.set("n", "go", "<cmd>Telescope lsp_type_definitions<cr>", opts)
-            vim.keymap.set("n", ".d", "<cmd>Telescope diagnostics<cr>", opts)
-        else
-            vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-            vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-            vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-            vim.keymap.set("n", "go", vim.lsp.buf.type_definition, opts)
-        end
-
-        if client.server_capabilities.documentFormattingProvider then
-            vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format, opts)
-        end
-
-        -- For disabling syntax highlighting from LSP
-        client.server_capabilities.semanticTokensProvider = nil
-    end)
-
+    lsp_zero.on_attach(require("common").lsp_on_attach)
     lsp_zero.setup_servers({ "clangd", "ocamllsp", "pyright" })
 
     require("mason").setup()
