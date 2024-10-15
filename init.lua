0cd9a3f4170b4f2fb637e35526305e5b159c82d5
Kevin Lee <klee03509@gmail.com>
2024-10-14 18:06:47 -0700

    2024-rewrite: Add entrypoint and install lazy.nvim

---
+++ init.lua
@@ -0,0 +1,41 @@
+local lazy_path = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+if not (vim.uv or vim.loop).fs_stat(lazy_path) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable",
+        lazy_path,
+    })
+end
+
+vim.opt.rtp:prepend(lazy_path)
+
+require("lazy").setup({
+    spec = {},
+    change_detection = { enabled = false },
+    performance = {
+        cache = { enabled = true },
+        rtp = {
+            disabled_plugins = {
+                "2html_plugin",
+                "getscript",
+                "getscriptPlugin",
+                "gzip",
+                "gzipPlugin",
+                "matchit",
+                "matchparen",
+                "tar",
+                "tarPlugin",
+                "tohtml",
+                "tutor",
+                "vimball",
+                "vimballPlugin",
+                "zip",
+                "zipPlugin",
+            },
+        },
+    },
+})
