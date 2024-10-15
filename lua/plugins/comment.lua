029f1630bcaebaa93675615e0236f0b5d3fd34f4
Kevin Lee <klee03509@gmail.com>
2024-10-15 04:01:36 -0700

    2024-rewrite: Add Comments.nvim for commenting code

---
+++ lua/plugins/comment.lua
@@ -0,0 +1,18 @@
+local M = {
+    "numToStr/Comment.nvim",
+    lazy = true,
+    event = "VeryLazy",
+}
+
+M.config = function()
+    require("Comment").setup({
+        padding = true,
+        mappings = {
+            basic = true,
+        },
+    })
+
+    require("Comment.ft").set("json", { "//%s", "/*%s*/" })
+end
+
+return M
