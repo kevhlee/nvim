1c95ff55e29fe67d2edc5af7d9043aa5ac0a991f
Kevin Lee <klee03509@gmail.com>
2024-10-14 23:02:21 -0700

    2024-rewrite: Add database client interface using nvim-dbee

---
+++ lua/plugins/database.lua
@@ -0,0 +1,27 @@
+local M = {
+    "kndndrj/nvim-dbee",
+    lazy = true,
+    event = "VeryLazy",
+    dependencies = {
+        { "MunifTanjim/nui.nvim" },
+    },
+}
+
+M.build = function()
+    require("dbee").install()
+end
+
+M.opts = function()
+    return {
+        -- See https://github.com/kndndrj/nvim-dbee?tab=readme-ov-file#specifying-connections to learn
+        -- how to set up connections through the environment.
+        sources = {
+            require("dbee.sources").EnvSource:new("DBEE_CONNECTIONS"),
+            require("dbee.sources").FileSource:new(
+                vim.fn.stdpath("cache") .. "/dbee/persistence.json"
+            ),
+        },
+    }
+end
+
+return M
