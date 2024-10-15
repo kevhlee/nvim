2ba4540840cb6caede7dbf11c3ee1821de70bf41
Kevin Lee <klee03509@gmail.com>
2024-10-14 21:51:30 -0700

    2024-rewrite: Use oil.nvim for file exploration

---
+++ lua/plugins/file-explorer.lua
@@ -0,0 +1,60 @@
+local M = {
+    "stevearc/oil.nvim",
+    lazy = true,
+    event = "VeryLazy",
+    dependencies = {
+        { "nvim-tree/nvim-web-devicons" },
+    },
+}
+
+M.opts = {
+    columns = {
+        "icon",
+        "permissions",
+    },
+    keymaps = {
+        ["<C-h>"] = "actions.show_help",
+        ["<C-r>"] = "actions.refresh",
+        ["<C-o>"] = "actions.open_external",
+        ["<C-v>"] = {
+            "actions.select",
+            opts = { vertical = true },
+            desc = "Open the entry in a vertical split",
+        },
+        ["<C-x>"] = {
+            "actions.select",
+            opts = { horizontal = true },
+            desc = "Open the entry in a horizontal split",
+        },
+
+        -- Remove these mappings
+        ["g?"] = false,
+        ["<C-s>"] = false,
+        ["<C-l>"] = false,
+        ["`"] = false,
+        ["~"] = false,
+        ["gs"] = false,
+        ["gx"] = false,
+        ["g."] = false,
+        ["g\\"] = false,
+    },
+    view_options = {
+        show_hidden = true,
+        sort = {
+            { "type", "asc" },
+            { "name", "asc" },
+        },
+    },
+}
+
+M.keys = {
+    {
+        "<C-n>",
+        function()
+            require("oil").toggle_float()
+        end,
+        desc = "(Oil) Toggle file explorer from current file",
+    },
+}
+
+return M
