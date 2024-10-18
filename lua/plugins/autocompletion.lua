5fceae78aead68221a2eee09a0e693dea14106de
Kevin Lee <klee03509@gmail.com>
2024-10-17 23:19:14 -0700

    2024-rewrite: Add autocompletions using nvim-cmp

---
+++ lua/plugins/autocompletion.lua
@@ -0,0 +1,46 @@
+local M = {
+    "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
+    dependencies = {
+        -- Utility
+        {
+            "VonHeikemen/lsp-zero.nvim",
+            branch = "v4.x",
+            lazy = true,
+            config = false,
+        },
+
+        -- Sources
+        { "hrsh7th/cmp-nvim-lsp" },
+        { "hrsh7th/cmp-buffer" },
+    },
+}
+
+M.config = function()
+    local cmp = require("cmp")
+    local cmp_action = require("lsp-zero").cmp_action()
+
+    cmp.setup({
+        sources = {
+            { name = "nvim_lsp" },
+            { name = "buffer" },
+        },
+        mapping = cmp.mapping.preset.insert({
+            ["<cr>"] = cmp.mapping.confirm({ select = false }),
+            ["<Tab>"] = cmp_action.tab_complete(),
+            ["<S-Tab>"] = cmp.mapping.select_prev_item({ behavior = "select" }),
+        }),
+        snippet = {
+            expand = function(args)
+                vim.snippet.expand(args.body)
+            end,
+        },
+        formatting = require("lsp-zero").cmp_format({ details = true }),
+        window = {
+            completion = cmp.config.window.bordered(),
+            documentation = cmp.config.window.bordered(),
+        },
+    })
+end
+
+return M
