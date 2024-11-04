7920397bb09628928a78244fed3884b09c32de5e
Kevin Lee <klee03509@gmail.com>
2024-11-03 18:13:57 -0800

    2025: Add lazy.nvim for plugin management

--- init.lua
+++ init.lua
@@ -2,3 +2,62 @@ require("globals")
 require("options")
 require("autocmd")
 require("keymaps")
+
+local lazy_path = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazy_repo = "https://github.com/folke/lazy.nvim.git"
+
+if not (vim.uv or vim.loop).fs_stat(lazy_path) then
+    local out = vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "--branch=stable",
+        lazy_repo,
+        lazy_path,
+    })
+
+    if vim.v.shell_error > 0 then
+        vim.api.nvim_echo({
+            { "Could not clone lazy.nvim:\n" .. out .. "\n", "ErrorMsg" },
+        }, true, {})
+        return
+    else
+    end
+end
+
+vim.opt.rtp:prepend(lazy_path)
+
+local spec = {
+    { import = "plugins" },
+}
+
+if
+    vim.fn.glob(vim.fn.stdpath("config") .. "/lua/override/plugins/*.lua") ~= ""
+then
+    table.insert(spec, { import = "override.plugins" })
+end
+
+require("lazy").setup({
+    spec = spec,
+    change_detection = {
+        notify = false,
+    },
+    install = {
+        colorscheme = { "default" },
+    },
+    performance = {
+        rtp = {
+            disabled_plugins = {
+                "editorconfig",
+                "gzip",
+                "matchit",
+                "matchparen",
+                "spellfile",
+                "tarPlugin",
+                "tohtml",
+                "tutor",
+                "zipPlugin",
+            },
+        },
+    },
+})
