150689a3344c818d86e0cbdbd075aa6b1ba2301d
Kevin Lee <klee03509@gmail.com>
2024-11-07 22:15:14 -0800

    2025: Add additional plugins for mini.nvim

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -52,7 +52,84 @@ indentscope.opts = {
     },
 }
 
+--
+-- Statusline
+--
+
+local statusline = {
+    "echasnovski/mini.statusline",
+    event = "UIEnter",
+}
+
+statusline.opts = function()
+    local MiniStatusline = require("mini.statusline")
+
+    return {
+        content = {
+            active = function()
+                -- stylua: ignore start
+                local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = 120 })
+                local git           = MiniStatusline.section_git({ trunc_width = 40 })
+                local diff          = MiniStatusline.section_diff({ trunc_width = 75 })
+                local filename      = MiniStatusline.section_filename({ trunc_width = 140 })
+                local fileinfo      = MiniStatusline.section_fileinfo({ trunc_width = 120 })
+                -- stylua: ignore end
+
+                return MiniStatusline.combine_groups({
+                    {
+                        hl = mode_hl,
+                        strings = { mode },
+                    },
+                    {
+                        hl = "MiniStatuslineDevinfo",
+                        strings = { git, diff },
+                    },
+                    "%<",
+                    {
+                        hl = "MiniStatuslineFilename",
+                        strings = { filename },
+                    },
+                    "%=", -- End left alignment
+                    {
+                        hl = "MiniStatuslineFileinfo",
+                        strings = { fileinfo },
+                    },
+                    {
+                        hl = mode_hl,
+                        strings = { "%l│%2v│%P" },
+                    },
+                })
+            end,
+        },
+    }
+end
+
+--
+-- Tabline
+--
+
+local tabline = {
+    "echasnovski/mini.tabline",
+    event = "UIEnter",
+}
+
+tabline.config = function()
+    local MiniTabline = require("mini.tabline")
+
+    MiniTabline.setup({
+        format = function(buf_id, label)
+            local suffix = vim.bo[buf_id].modified and "+ " or ""
+            return MiniTabline.default_format(buf_id, label) .. suffix
+        end,
+        tabpage_section = "right",
+    })
+
+    vim.api.nvim_set_hl(0, "MiniTablineFill", { fg = "None", bg = "None" })
+end
+
 return {
     hipatterns,
     indentscope,
+    statusline,
+    tabline,
 }
