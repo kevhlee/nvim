37df98ed2e28d6ea661aeb053af2a0d067dbd54b
Kevin Lee <klee03509@gmail.com>
2024-11-07 22:44:51 -0800

    2025: Don't really need lsp.lua

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,3 +1,36 @@
+--- @class LspConfigSpec
+--- @field config? vim.lsp.ClientConfig
+--- @field skip boolean
+
+--- @param defaults vim.lsp.ClientConfig
+--- @return table<string, LspConfigSpec>
+local function get_config_specs(defaults)
+    local specs = {}
+    local ok, override = pcall(require, "override.lsp")
+    if ok and type(override) == "table" then
+        for name, get_spec in pairs(override) do
+            specs[name] = get_spec(defaults)
+        end
+    end
+    return specs
+end
+
+--- @param client vim.lsp.Client
+--- @param bufnr integer
+local function default_on_attach(client, bufnr)
+    local opts = { buffer = bufnr }
+
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+    vim.keymap.set("n", "go", vim.lsp.buf.type_definition, opts)
+    vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+    vim.keymap.set("n", "gs", vim.lsp.buf.signature_help, opts)
+    vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, opts)
+    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+end
+
 local M = {
     "neovim/nvim-lspconfig",
     dependencies = {
@@ -23,10 +56,10 @@ M.config = function()
     local defaults =
         vim.tbl_deep_extend("force", require("lspconfig").util.default_config, {
             capabilities = require("cmp_nvim_lsp").default_capabilities(),
-            on_attach = require("lsp").default_on_attach,
+            on_attach = default_on_attach,
         })
 
-    local specs = require("lsp").get_config_specs(defaults)
+    local specs = get_config_specs(defaults)
 
     require("mason").setup()
 
