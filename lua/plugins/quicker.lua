d1fac11731b3872d48fc23554494920335bbcc12
Kevin Lee <kevin.lee@liferay.com>
2024-11-15 14:26:21 -0800

    2025: Add Quicker to make working with the quickfix/location lists easier

---
+++ lua/plugins/quicker.lua
@@ -0,0 +1,62 @@
+local M = {
+    "stevearc/quicker.nvim",
+    event = "FileType qf",
+}
+
+M.opts = {
+    highlight = {
+        lsp = false,
+        load_buffers = false,
+    },
+    keys = {
+        {
+            "<",
+            function()
+                require("quicker").collapse()
+            end,
+            desc = "(Quicker) Collapse quickfix context",
+        },
+        {
+            ">",
+            function()
+                require("quicker").expand()
+            end,
+            desc = "(Quicker) Expand quickfix context",
+        },
+        {
+            "<C-l>",
+            function()
+                require("quicker").refresh()
+            end,
+            desc = "(Quicker) Refresh quickfix/location list",
+        },
+    },
+}
+
+M.keys = {
+    {
+        "<leader>q",
+        function()
+            require("quicker").toggle({
+                focus = true,
+                min_height = 10,
+                max_height = 10,
+                -- See `:h nvim_parse_cmd()` for more information on
+                -- `open_cmd_mods`.
+                open_cmd_mods = {
+                    split = "botright",
+                },
+            })
+        end,
+        desc = "(Quicker) Toggle quickfix list",
+    },
+    {
+        "<leader>l",
+        function()
+            require("quicker").toggle({ focus = true, loclist = true })
+        end,
+        desc = "(Quicker) Toggle location list",
+    },
+}
+
+return M
